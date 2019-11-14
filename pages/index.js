6002737541fdef24e7269d19d231b788e36c6651
Alex Stanciu <alex@auth0.com>
2019-11-14 12:20:13 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Homes</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+
+
+    <div className='hero'>
+      <h1 className='title'>ƒ.cc</h1>
+      <p className='description'>
+        
+      </p>
+
+
+    </div>
+
+
+    <style jsx>{`
+ 
+     
+    `}</style>
+
+    <style jsx>{`
+         :global(body) {
+          @import url('https://fonts.googleapis.com/css?family=Lora&display=swap');
+          
+          margin: 0;
+          font-family: font-family: 'Lora', serif;      
+          background-color: #2c2e33;
+        }
+        
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 100px;
+        font-weight: 400;
+        color: #e1b400;
+        text-align: center;
+      }
+     
+
+
+    `}</style>
+
+    
+  </div>
+)
+
+export default Home
